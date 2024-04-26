3f0bf4c587b1297a7f95db3ecd0ecaacfb95da4b
Alexandre Cesar <alexandre.cesar.1995@hotmail.com>
2024-04-25 21:53:35 -0300

    Inicio html e java form hospital

---
+++ Cirurgico/scripts/scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('bt-apagar').addEventListener('click', apagar);
+document.getElementById('bt-gravar').addEventListener('click', gravar);
+document.getElementById('bt-novo').addEventListener('click', limparForm);
