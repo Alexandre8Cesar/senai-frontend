d3a6825ce451fa40141f68b88f8bbbf34a995d24
Alexandre Cesar <alexandre.cesar.1995@hotmail.com>
2024-05-21 20:02:18 -0300

    carregar lista json, alteração html e css

--- site-pizzaria/scripts/script.js
+++ site-pizzaria/scripts/script.js
@@ -4,6 +4,23 @@ let show = true;
 
 menutoggle.addEventListener('click', () => {
     document.body.style.overflow = show ? 'hidden' : 'intial';
-    menuContent.classList.toggle ('on', show);
+    menuContent.classList.toggle('on', show);
     show = !show;
 })
+
+let itensCardapio = document.querySelector(".itens-cardapio");
+for (const p of produtos) {
+    itensCardapio.innerHTML += `
+        <div class="">
+            <img src="${p.img}" alt="${p.dsImg}">
+                <div class="info">
+                    <h3></h3>
+                    <h4>6 fatias<span>R$${p.fatias6}</span></h4>
+                    <h4>8 fatias<span>R$${p.fatias8}</span></h4>
+                    <h4>12 fatias<span>R$${p.fatias12}</span></h4>
+                    <button class="pedir">pedir agora</button>
+
+                </div>
+                </div>
+                `;
+}
